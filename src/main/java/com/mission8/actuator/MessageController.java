a88760bff795e754d3c1e7e169b7ec3da4ee7b4d
Cheah How Ong <cheah.how.ong@accenture.com>
2020-03-03 16:56:55 +0800

    1st commit

---
+++ src/main/java/com/mission8/actuator/MessageController.java
@@ -0,0 +1,21 @@
+package com.mission8.actuator;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class MessageController {
+    @Value("${message}")
+    private String message;
+
+    @Value("${title}")
+    private String title;
+
+    @RequestMapping("/message")
+    String getMessage() {
+        return message + " " + title;
+    }
+}
